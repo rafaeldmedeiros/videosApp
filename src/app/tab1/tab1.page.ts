001a2824f7cd206f19662b8b53d21a3268720c01
Rafael Silva <r.medeiros@conductor.com.br>
2023-03-21 22:33:51 -0300

    Serviço temporário e envio de dados do filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,7 +1,9 @@
+import { DadosService } from './../services/dados.service';
 import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -15,24 +17,36 @@ export class Tab1Page {
   titulo = "Room Mate";
   listaVideos: IFilme[] = [
     {
-      nome:'Batem à Porta',
-      lancamento:'2023',
-      duracao:'1h 40m',
+      nome: 'Batem à Porta',
+      lancamento: '2023',
+      duracao: '1h 40m',
       classificacao: 76,
       cartaz: 'https://www.themoviedb.org/t/p/w440_and_h660_face/xtLEm7icyupihsdhUYXJdQ7sKFo.jpg',
-      generos: ['Terror', 'Mistério', 'Thriller']
+      generos: ['Terror', 'Mistério', 'Thriller'],
+      pagina: '/mortal-kombat'
     },
     {
-      nome:'Black Panther: Wakanda Para Sempre',
-      lancamento:'2022',
-      duracao:'2h 41m',
+      nome: 'Black Panther: Wakanda Para Sempre',
+      lancamento: '2022',
+      duracao: '2h 41m',
       classificacao: 80,
       cartaz: 'https://www.themoviedb.org/t/p/w440_and_h660_face/nZ69WTv7n01womaNz3SHa4inA9x.jpg',
-      generos: ['Ação', 'Aventura', 'Ficção científica']
+      generos: ['Ação', 'Aventura', 'Ficção científica'],
+      pagina: '/liga-justica'
     }
   ];
 
-  constructor(private alertController: AlertController, private toastController: ToastController) {}
+  constructor(
+    private alertController: AlertController,
+    private toastController: ToastController,
+    public dadosService: DadosService,
+    public route: Router) { }
+
+  exibirFilme(filme: IFilme) {
+    this.dadosService.guardarDados('filme', filme);
+    this.route.navigateByUrl('/dados-filme');
+  }
+
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
